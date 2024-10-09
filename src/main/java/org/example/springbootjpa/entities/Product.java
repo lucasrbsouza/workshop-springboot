493a5695ec6e7407f780c4bd32d6babe8d13ddd1
e <joselucastrbssilvasouza@gmail.com>
2024-10-09 10:05:41 -0300

    refatorando a classe Product

--- src/main/java/org/example/springbootjpa/entities/Product.java
+++ src/main/java/org/example/springbootjpa/entities/Product.java
@@ -1,5 +1,6 @@
 package org.example.springbootjpa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -37,6 +38,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -46,5 +50,14 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 }
